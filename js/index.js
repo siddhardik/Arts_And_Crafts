0bca47ecc6a67b1d6effd17c476ed1ace52e789d
siddhardik <siddharthabhunia2001@gmail.com>
2023-05-09 18:05:41 +0530

    Image Resize

--- js/index.js
+++ js/index.js
@@ -1,40 +1,112 @@
-const products=[
-    {
-        id:0,
-        image:'images/img1.jpg',
-        title:'Image 1',
-        price:187
-    },
-    {
-        id:1,
-        image:'images/img2.jpg',
-        title:'Image 2',
-        price:190
-    },
-    {
-        id:2,
-        image:'images/img3.jpg',
-        title:'thakur',
-        price:139
-    },
-    {
-        id:3,
-        image:'images/img4.jpg',
-        title:'Image 4',
-        price:15
-    },
-    {
-        id:4,
-        image:'images/img1.jpg',
-        title:'Image 5',
-        price:178
-    },
-    {
-        id:5,
-        image:'images/img4.jpg',
-        title:'Image 6',
-        price:1670
-    }
+const products = [
+  {
+    id: 0,
+    image: 'images/a1.jpg',
+    title: 'Bengal Handicrafts & Handlooms Decorative Wall_Hanging Navadurga murti on 14X12 Inch Bamboo_kulo',
+    price: 1299
+  },
+  {
+    id: 1,
+    image: 'images/b1.jpg',
+    title: 'Bengal Handicrafts & Handlooms Natural Shola Chandmala for Durga Puja, Chand mala for Laxmi Puja',
+    price: 599
+  },
+  {
+    id: 2,
+    image: 'images/c1.jpg',
+    title: 'Breeze Handicrafts Wooden Showpiece Bankura Horse Statue for Home Decoration West Bengal Souvenir ',
+    price: 975
+  },
+  {
+    id: 3,
+    image: 'images/d1.jpg',
+    title: 'Bengal Handicrafts & Handlooms Braided Jute Placemats, Set of 2. Best for Bed-Side Table/Centre',
+    price: 399
+  },
+  {
+    id: 4,
+    image: 'images/e1.jpg',
+    title: 'Bengal Handicrafts & Handlooms Bamboo Circular Pendent Lamp Shades (Brown, 8 Inch) ',
+    price: 475
+  },
+  {
+    id: 5,
+    image: 'images/f1.jpg',
+    title: 'Handicrafts Paradise Banithani Painted On Marble Pate 9 Inch',
+    price: 1295
+  },
+
+  {
+    id: 6,
+    image: 'images/g1.jpg',
+    title: 'Karukala Handicraft Home Decor 11 Inch by 7.5 Inch Terracotta Jamini Roy Durga MATA Fig',
+    price: 999
+  },
+
+  {
+    id: 7,
+    image: 'images/h1.jpg',
+    title: 'Bengal Handicrafts & Handlooms Decorative Bamboo Pendent Lamp Shades ',
+    price: 699
+  },
+
+  {
+    id: 8,
+    image: 'images/i1.jpg',
+    title: 'Soul of Bengal - Women Hand Embroidered Kantha Stitch Cotton Blouse Material 1 Meter' ,
+    price: 445
+  },
+
+  {
+    id: 9,
+    image: 'images/j1.jpg',
+    title: 'AVIKA All India Handicrafts Bamboo Eco-Friendly Multiutility Wicker FarmerS Basket ',
+    price: 599
+  },
+
+  {
+    id: 10,
+    image: 'images/k1.jpg',
+    title: 'Bengal Handicrafts & Handlooms Braided Jute Placemats, 41 cm Round, Best for Bed-Side Table ',
+    price: 429
+  },
+
+  {
+    id: 11,
+    image: 'images/l1.jpg',
+    title: 'Bengal Handicrafts & Handlooms Jute Placemat. Dining Table Mate Items or Dining Table Runner.  ',
+    price: 149
+  },
+
+  {
+    id: 12,
+    image: 'images/m1.jpg',
+    title: 'Karukala Handicraft Home Decor 12 Inch by 9 Inch Terracotta Metallic Dancing Figure Art of Bengal',
+    price: 949
+  },
+
+  {
+    id: 13,
+    image: 'images/n1.jpg',
+    title: 'Bengal Handicrafts & Handlooms Handmade Bamboo Tea/Coffee/Beer Mug. Set of 2, 250ml (Natural Golden',
+    price: 277
+  },
+
+  {
+    id: 14,
+    image: 'images/o1.jpg',
+    title: 'Bengal Handicrafts & Handlooms Decorative Natural Bamboo Wall Hanging Flower Vase. (2, 13 Inch),',
+    price: 299
+  },
+
+  {
+    id: 15,
+    image: 'images/p1.jpg',
+    title: 'Home Decor Item “Deer Head”45 cm high with Horn – Wooden Handicraft showpieces Product for Wall',
+    price: 999
+  }
+
+
 ]
 
 
@@ -47,7 +119,7 @@ function showProducts() {
       <img src="${product_object.image}" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">${product_object.title}</h5>
-        <p class="card-text"> Price : ${product_object.price}</p>
+        <p class="card-text"> <b>Price : ₹${product_object.price}</b></p>
         <a  id= ${index} onclick="addToCart(${product_object.id})" class="btn btn-primary">Add to Cart</a>
       </div>
     </div>`;
